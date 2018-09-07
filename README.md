ca89a51d2fcf633ef6810ad327478128e669682f
jooseppi12 <urijozsef@europe.com>
2018-09-07 11:37:36 +0200

    Initial commit for IntroJS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper IntroJS
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for IntroJS.
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/IntroJS)
+* [See it running live](https://websharper-samples.github.io/IntroJS)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,41 @@
-TODO Replace me
+namespace Samples
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Html
+open WebSharper.UI.Client
+open WebSharper.IntroJS
+
+[<Require(typeof<WebSharper.IntroJS.Resources.MainTheme>)>]
+[<JavaScript>]
+module HelloWorld =
+    
+    type IndexTemplate = Templating.Template<"wwwroot/index.html">
+
+    let People =
+        ListModel.FromSeq [
+            "John"
+            "Paul"
+        ]
+
+    [<SPAEntryPoint>]
+    let Main () =
+        let newName = Var.Create ""
+
+        IndexTemplate.Main()
+            .ListContainer(
+                People.View.DocSeqCached(fun (name: string) ->
+                    IndexTemplate.ListItem().Name(name).Doc()
+                )
+            )
+            .Name(newName)
+            .Add(fun _ ->
+                People.Add(newName.Value)
+                newName.Value <- ""
+            )
+            .Intro(fun _ ->
+                IntroJS.IntroJs().Start() |> ignore
+            )
+            .Doc()
+        |> Doc.RunById "main"
